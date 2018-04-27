808735f079c385dbc410f37231d0e697ae968d9e
Amal-aa1506550 <aa1506550@qu.edu.qa>
2018-04-27 23:02:00 +0300

    V1-HW4

---
+++ tutorial5/Assets/Scripts/Pickup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+    public GameObject onhand;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnMouseDown() //mouse clicked
+    {
+        rb.useGravity = false;
+        rb.transform.position = onhand.transform.position;
+        rb.transform.parent = GameObject.Find("FBSController").transform;
+        rb.transform.parent = GameObject.Find("FirstPersonCharacter").transform;
+    }
+    void OnMouseUpAsButton()
+    {
+        rb.transform.parent = null;
+        rb.useGravity = true;
+    }
+
+}
