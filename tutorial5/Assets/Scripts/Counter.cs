4002d16db8d2c847d5f1f2397266d2392c4f2290
Amal-aa1506550 <aa1506550@qu.edu.qa>
2018-04-28 21:33:06 +0300

    9:30v-hw4

---
+++ tutorial5/Assets/Scripts/Counter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour {
+	public Text text;
+	public int counter;
+	public Rigidbody fireball;
+
+
+	// Use this for initialization
+	void Start () {
+		counter = 0;
+		text.text = "count: " + counter.ToString ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void onTriggerEnter(Collider other)
+	{ 
+		if (other.CompareTag ("twig")) {
+
+			counter++;
+			text.text = "count: " + counter.ToString ();
+
+
+		}
+
+
+	}
+}
